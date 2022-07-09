5fbe3d1922a949e1e06b09754291b05e97962980
Leonardo Jales <bandrosh@gmail.com>
2022-07-09 14:03:50 -0300

    Initial Commit Project

---
+++ src/main/java/br/com/retailstore/wishlist/RetailStoreWishlistApplication.java
@@ -0,0 +1,13 @@
+package br.com.retailstore.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailStoreWishlistApplication.class, args);
+    }
+
+}
