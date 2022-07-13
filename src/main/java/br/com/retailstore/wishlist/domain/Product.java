cd4556782b319aab35018a67a445aeff9537fa64
Leonardo Jales <bandrosh@gmail.com>
2022-07-13 13:27:34 -0300

    Prevent receive empty values, client and product

--- src/main/java/br/com/retailstore/wishlist/domain/Product.java
+++ src/main/java/br/com/retailstore/wishlist/domain/Product.java
@@ -1,9 +1,14 @@
 package br.com.retailstore.wishlist.domain;
 
+import br.com.retailstore.wishlist.exception.EmptyValueException;
+
 import java.util.Objects;
 
 public record Product(String id) {
     public Product {
         Objects.requireNonNull(id);
+        if(id.isEmpty()) {
+            throw new EmptyValueException("Product value is empty.");
+        }
     }
 }
